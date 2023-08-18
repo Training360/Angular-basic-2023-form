cc3ec5634e02f057ee878b70a3c4e3c7dd4c8ac1
cherryApp <j.cserko@gmail.com>
2023-08-18 14:44:03 +0200

    start lesson-09

--- src/app/ticket/ticket-editor/ticket-editor.component.ts
+++ src/app/ticket/ticket-editor/ticket-editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, inject, numberAttribute } from '@angular/core';
+import { Router } from '@angular/router';
+import { Ticket } from 'src/app/model/ticket';
 import { TicketService } from 'src/app/service/ticket.service';
 
 @Component({
@@ -10,6 +12,8 @@ export class TicketEditorComponent implements OnInit {
 
   ticketService = inject(TicketService);
 
+  router = inject(Router);
+
   @Input({transform: numberAttribute, alias: 'id'}) ticketID = 0;
 
   ticket$ = this.ticketService.one$;
@@ -18,5 +22,11 @@ export class TicketEditorComponent implements OnInit {
     if (this.ticketID) {
       this.ticketService.dispatch('get', this.ticketID);
     }
+  }
+
+  onUpdate(ticket: Ticket): void {
+    this.ticketService.update(ticket).subscribe(
+      updatedTicket => this.router.navigate(['/ticket'])
+    );
   }
 }
